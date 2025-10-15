871ee95bc0292708dfb246ecae14ee3a4b29eb5d
Marylle Laguna <maryllelagunaa@gmail.com>
2025-10-15 17:36:09 +0800

    Supabase Starter and Next Config Setup

---
+++ brain-bento/next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: "C:\\Users\\Dell\\Documents\\marylle\\GitHub repos\\websys\\batch-2025-brain-bento-web\\brain-bento",
+  },
+};
+
+module.exports = nextConfig;
