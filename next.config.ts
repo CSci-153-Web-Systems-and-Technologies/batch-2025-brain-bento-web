321c68866ac54ab6dc8a85567020845f4c872dba
Marylle Laguna <maryllelagunaa@gmail.com>
2025-12-06 20:12:09 +0800

    fix to correct middleware logic for session handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: "C:\\Users\\Dell\\Documents\\marylle\\GitHub repos\\websys\\batch-2025-brain-bento-web\\brain-bento",
+    root: "C:\\projects\\1\\batch-2025-brain-bento-web",
   },
 };
 
